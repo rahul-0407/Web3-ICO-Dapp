93e1301b358685dc0b13d863bab8940032f4d0d3
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-11-25 16:07:38 +0530

    ERC2O contract completed

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -2,3 +2,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Razer is ERC20{
+    constructor() ERC20("Razer", "@RZR") {
+        _mint(msg.sender, 10000000000000000000000000);
+    }
+}
+
