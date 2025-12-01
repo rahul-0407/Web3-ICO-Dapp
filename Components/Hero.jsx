40ea97eb9459f22085e15488a2cdcfd86bf874e4
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-02 02:22:23 +0530

    hero section completed

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = ({setBuyModel, account, CONNECT_WALLET, setAccount, setLoader, deta
 
   const [percentage, setPercentage] =  useState()
 
-  useInsertionEffect(()=> {
+  useEffect(()=> {
     const calculatePercentage = () =>  {
       const tokenSold = detail?.soldTokens ?? 0;
       const tokenTotalSupply = detail?.soldTokens + Number(detail?.tokenBal) * 1 ?? 1;
@@ -86,11 +86,62 @@ const Hero = ({setBuyModel, account, CONNECT_WALLET, setAccount, setLoader, deta
           </div>
         </div>
 
-        <div className="col-lg-5"></div>
+        <div className="col-lg-5">
+          <div className="hero__explore-wrap text-center">
+            <div className="hero__explore text-center">
+              <div className="scroll-down"/>
+              <span>Explore Causes</span>
+            </div>
+
+            <div className="hero__countdown">
+              <h6 className="text-center">
+                ICO Will Start in...
+              </h6>
+            </div>
+
+
+          </div>
+        </div>
 
 
       </div>
     </div>
+
+    <div className="hero__shape">
+      <div className="shape shape--1">
+        <img src="assets/img/shape/h_shape.png" alt="" />
+      </div>
+      <div className="shape shape--2">
+        <img src="assets/img/shape/h_shape2.png" alt="" />
+      </div>
+      <div className="shape shape--3">
+        <img src="assets/img/shape/h_shape3.png" alt="" />
+      </div>
+    </div>
+
+    
+    <div className="hero__coin">
+      <div className="coin coin--1">
+        <img src="assets/img/icon/coin1.png" alt="" />
+      </div>
+      <div className="coin coin--2">
+        <img src="assets/img/icon/coin2.png" alt="" />
+      </div>
+      <div className="coin coin--3">
+        <img src="assets/img/icon/coin3.png" alt="" />
+      </div>
+      <div className="coin coin--4">
+        <img src="assets/img/icon/coin4.png" alt="" />
+      </div>
+      <div className="coin coin--5">
+        <img src="assets/img/icon/coin5.png" alt="" />
+      </div>
+      <div className="coin coin--6">
+        <img src="assets/img/icon/coin6.png" alt="" />
+      </div>
+    </div>
+
+
   </section>;
 };
 
