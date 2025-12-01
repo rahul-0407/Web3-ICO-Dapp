1ee5adb5c508d8b225d8d07739634f3717b26a84
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-01 22:37:57 +0530

    header completed

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,35 +1,44 @@
 import React from "react";
 
-const Header = ({account, setAccount, CONNECT_WALLET,setLoader, setOwnerModel, shortenAddress, detail, currency, ownerModel}) => { 
+const Header = ({
+  account,
+  setAccount,
+  CONNECT_WALLET,
+  setLoader,
+  setOwnerModel,
+  shortenAddress,
+  detail,
+  currency,
+  ownerModel,
+}) => {
+  const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(false);
 
-  const[isMetamaskInstalled, setIsMetamaskInstalled] = useState(false)
-
-  useEffect(()=>{
-    if(typeof window.ethereum !== "undefined"){
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
       setIsMetamaskInstalled(true);
 
-      window.ethereum.on("accountsChanged",handleAccountsChanged)
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
     }
 
-    return()=> {
-      if(typeof window.ethereum !== "undefined"){
+    return () => {
+      if (typeof window.ethereum !== "undefined") {
         window.ethereum.removeListener(
           "accountsChanged",
           handleAccountsChanged
-        )
+        );
       }
-    }
-  },[])
+    };
+  }, []);
 
   const handleAccountsChanged = (accounts) => {
-    setAccount(accounts[0])
-  }
+    setAccount(accounts[0]);
+  };
 
   const connectMetamask = async () => {
-    if(typeof window.ethereum !== "undefined"){
+    if (typeof window.ethereum !== "undefined") {
       try {
         const accounts = await window.ethereum.request({
-          method:"eth_requestAccounts"
+          method: "eth_requestAccounts",
         });
         setAccount(accounts[0]);
       } catch (error) {
@@ -38,28 +47,97 @@ const Header = ({account, setAccount, CONNECT_WALLET,setLoader, setOwnerModel, s
     } else {
       console.log("Metamask is not installed");
     }
-  }
+  };
 
-  return <header className="site-header header--transparent ico-header ">
-    <div className="header__main-wrap">
-      <div className="container mxw_1640">
-        <div className="header__main ul_ui_between">
-          <div className="header_left ul_ui">
-            <div className="header__logo">
-              <a href="/" >
-              <img src="assets/img/logo/logo.svg" alt="" srcSet="" />
-              </a>
+  return (
+    <header className="site-header header--transparent ico-header ">
+      <div className="header__main-wrap">
+        <div className="container mxw_1640">
+          <div className="header__main ul_ui_between">
+            <div className="header_left ul_ui">
+              <div className="header__logo">
+                <a href="/">
+                  <img src="assets/img/logo/logo.svg" alt="" srcSet="" />
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="main-menu__wrap ul_ui navbar navbar-expand-xl">
-            <nav className="main-menu collapse navbar-collapse">
-              <ul></ul>
-            </nav>
+            <div className="main-menu__wrap ul_ui navbar navbar-expand-xl">
+              <nav className="main-menu collapse navbar-collapse">
+                <ul>
+                  <li className="active has-mega-menu">
+                    <a href="/">Home</a>
+                  </li>
+                  <li>
+                    <a className="scrollspy-btn" href="#about">
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a className="scrollspy-btn" href="#roadmap">
+                      Roadmap
+                    </a>
+                  </li>
+                  <li>
+                    <a className="scrollspy-btn" href="#team">
+                      Team
+                    </a>
+                  </li>
+                  <li>
+                    <a className="scrollspy-btn" href="#faq">
+                      Faq
+                    </a>
+                  </li>
+                  <li>
+                    <a className="scrollspy-btn" href="#contact">
+                      Contact
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="scrollspy-btn"
+                      style={{ cursor: "pointer" }}
+                      onClick={() =>
+                        ownerModel ? setOwnerModel(false) : setOwnerModel(true)
+                      }
+                    >
+                      Tools
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="header__action ul_li">
+              <div className="d-xl-none">
+                <a className="header__bar hamburger_menu">
+                  <div className="header__bar-icon">
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                  </div>
+                </a>
+              </div>
+              {account?
+              <div className="header_account">
+                <a onClick={()=>  navigator.clipboard.writeText(detail?.address)}>{
+                  shortenAddress(detail?.address)
+                  }:{detail?.maticBal.slice(0,6)
+                  }{currency}</a>
+              </div>
+              :
+              <div className="header__account">
+                <a onClick={()=>connectMetamask()}>Connect Wallet</a>
+              </div>
+              }
+            </div>
+
+
           </div>
         </div>
       </div>
-    </div>
-  </header>;
+    </header>
+  );
 };
 
 export default Header;
