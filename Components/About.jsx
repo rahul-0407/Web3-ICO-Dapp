fe316c153a8411c8f74a193bebdfb721b1efae12
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-03 01:31:08 +0530

    working on about section

--- Components/About.jsx
+++ Components/About.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return <section id="about" className="about pos-rel pb-140">
+    <div className="container">
+      <div className="row align-items-center mt-none-30">
+        
+      </div>
+    </div>
+  </section>;
 };
 
 export default About;
