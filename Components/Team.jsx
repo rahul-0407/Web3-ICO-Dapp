112298dbb73c3ed79c57cb29efafd60ce6b7014c
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-02 13:36:24 +0530

    start building team section

--- Components/Team.jsx
+++ Components/Team.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialGithub,
+  TiSocialTwitter,
+  TiSocialInstagram,
+} from "react-icons/ti";
+import {FaPlus} from "react-icons/fa6"
 
 const Team = () => {
-  return <div>Team</div>;
+  return <section id="team" className="team pos-rel">Team</section>;
 };
 
 export default Team;
