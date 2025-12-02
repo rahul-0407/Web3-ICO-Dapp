9e099368acc33d76ec9461bdf2069f4ca78cac9d
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-02 13:34:17 +0530

    faq completed

--- Components/Faq.jsx
+++ Components/Faq.jsx
@@ -5,9 +5,116 @@ const Faq = () => {
     <div className="container">
       <div className="sec-title text-center mb-35">
         <h5 className="sec-title__subtitle">FAQ</h5>
-        <h2 className="sec-title__title"></h2>
+        <h2 className="sec-title__title">Frequently asked questions</h2>
+      </div>
+
+      <div className="faq__wrap">
+        <ul className="accordion_box clearfix">
+          <li className="accordion block active-block">
+            <div className="acc-btn">
+              <span>QA: 01</span> Type your questions about your projects of ICO?
+            </div>
+            <div className="acc_body current">
+              <div className="content">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur dolorem aperiam, voluptatibus quae repudiandae dicta quisquam possimus non ad.</p>
+                <ul>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                </ul>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, enim commodi illum sint magni nobis voluptates eligendi? Consectetur,<br/> <br/> nemo perferendis dolorum commodi, ratione tenetur, natus maiores saepe nulla repudiandae totam?
+                </p>
+              </div>
+            </div>
+          </li>
+          <li className="accordion block">
+            <div className="acc-btn">
+              <span>QA: 03</span> Type your questions about your projects of ICO?
+            </div>
+            <div className="acc_body">
+              <div className="content">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur dolorem aperiam, voluptatibus quae repudiandae dicta quisquam possimus non ad.</p>
+                <ul>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                </ul>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, enim commodi illum sint magni nobis voluptates eligendi? Consectetur,<br/> <br/> nemo perferendis dolorum commodi, ratione tenetur, natus maiores saepe nulla repudiandae totam?
+                </p>
+              </div>
+            </div>
+          </li>
+          <li className="accordion block">
+            <div className="acc-btn">
+              <span>QA: 04</span> Type your questions about your projects of ICO?
+            </div>
+            <div className="acc_body">
+              <div className="content">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur dolorem aperiam, voluptatibus quae repudiandae dicta quisquam possimus non ad.</p>
+                <ul>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                </ul>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, enim commodi illum sint magni nobis voluptates eligendi? Consectetur,<br/> <br/> nemo perferendis dolorum commodi, ratione tenetur, natus maiores saepe nulla repudiandae totam?
+                </p>
+              </div>
+            </div>
+          </li>
+          <li className="accordion block">
+            <div className="acc-btn">
+              <span>QA: 05</span> Type your questions about your projects of ICO?
+            </div>
+            <div className="acc_body">
+              <div className="content">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur dolorem aperiam, voluptatibus quae repudiandae dicta quisquam possimus non ad.</p>
+                <ul>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                </ul>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, enim commodi illum sint magni nobis voluptates eligendi? Consectetur,<br/> <br/> nemo perferendis dolorum commodi, ratione tenetur, natus maiores saepe nulla repudiandae totam?
+                </p>
+              </div>
+            </div>
+          </li>
+          <li className="accordion block">
+            <div className="acc-btn">
+              <span>QA: 02</span> Type your questions about your projects of ICO?
+            </div>
+            <div className="acc_body">
+              <div className="content">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur dolorem aperiam, voluptatibus quae repudiandae dicta quisquam possimus non ad.</p>
+                <ul>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                </ul>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, enim commodi illum sint magni nobis voluptates eligendi? Consectetur,<br/> <br/> nemo perferendis dolorum commodi, ratione tenetur, natus maiores saepe nulla repudiandae totam?
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+
+    <div className="faq__sec-shape">
+      <div className="shape shape-1">
+        <img src="assets/img/shape/s_shape1.png" alt="" />
+      </div>
+      <div className="shape shape-2">
+        <img src="assets/img/shape/s_shape2.png" alt="" />
       </div>
     </div>
+
+    
   </section>;
 };
 
