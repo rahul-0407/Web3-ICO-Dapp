f5dc9196faaed6ce58d789903c553890888a6410
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-02 14:07:57 +0530

    tokenInfo completed

--- Components/TokenInfo.jsx
+++ Components/TokenInfo.jsx
@@ -9,11 +9,66 @@ const TokenInfo = ({detail,currency}) => {
             <h5 className="sec-title__subtitle">
               ICO coindox Token
             </h5>
-            <h2 className="sec-title__title"></h2>
+            <h2 className="sec-title__title">
+              ICO Token <br/> Details and sale
+            </h2>
           </div>
         </div>
       </div>
+
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="token-info__img">
+            <img src="assets/img/token/t_info_img.png" alt="" />
+          </div>
+        </div>
+        <div className="col-lg-8">
+          <div className="token-info--info-wrap ul_li">
+            <ul className="token-info__list token-info--start">
+              <li>
+                <h4>Total Supply</h4>
+                <span>{detail?.supply}{detail?.symbol}</span>
+              </li>
+              <li>
+                <h4>Sold Token</h4>
+                <span>{Number(detail?.soldTokens)}{detail?.symbol}</span>
+              </li>
+              <li>
+                <h4>Minimal Transaction</h4>
+                <span>10 Token / Transaction</span>
+              </li>
+            </ul>
+            <ul className="token-info__list token-info--end">
+              <li>
+                <h4>Token Market Value</h4>
+                <span>{Number(detail?.supply)*Number(detail?.tokenPrice)} {currency}</span>
+              </li>
+              <li>
+                <h4>Fund Raised</h4>
+                <span>{Number(detail?.soldTokens)*Number(detail?.tokenPrice)} {currency}</span>
+              </li>
+              <li>
+                <h4>Acceptable Currency</h4>
+                <span>ETH, BTC, TLC, POL</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
     </div>
+
+
+    <div className="token-info__shape">
+      <div className="shape shape-1">
+        <img src="assets/img/shape/ti_shape1.png" alt="" />
+      </div>
+      <div className="shape shape-2">
+        <img src="assets/img/shape/ti_shape2.png" alt="" />
+      </div>
+    </div>
+
+
   </section>;
 };
 
