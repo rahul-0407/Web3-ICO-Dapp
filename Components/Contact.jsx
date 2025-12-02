5d7a67cb00194f2f8098bfaa57ef5f14c6edbae1
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-02 12:50:14 +0530

    working on contact section

--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import toast from "react-hot-toast";
+import { useForm } from "@formspree/react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const notifySuccess = (msg) => toast.success(msg, { duration: 2000 });
+  const notifyError = (msg) => toast.error(msg, { duration: 2000 });
+
+  const [state, handleSubmit] = useForm()
+
+  if(state.succeeded){
+    notifySuccess("successfully submitted");
+    window.location.reload()
+  }
+
+  return <section id="contact" className="ico-contact pos-rel">
+    
+  </section>;
 };
 
 export default Contact;
