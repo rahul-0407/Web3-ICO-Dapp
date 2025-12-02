5a51c86d12c35f67f7b53881a60b4028b97abb88
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-03 02:23:49 +0530

    owner completed

--- Components/Owner.jsx
+++ Components/Owner.jsx
@@ -1,23 +1,161 @@
 import React from "react";
-import {FaPlus} from "react-icons/fa6"
+import { FaPlus } from "react-icons/fa6";
 
-const Owner = ({setOwnerModel, currency, detail, account, setTransferModel, setTransferCurrency, setOpenDonate, TOKEN_WITHDRAW, setOpenUpdateAddress, setOpenUpdatePrice}) => {
-  return <section className="team pos-rel">
-    <div className="container">
-      <div className="new-owner team__wrap ul_li">
-        <div className="team__item">
-          <div className="avatar">
-            <img src="assets/img/shape/c_shape1.png" alt="" />
+const Owner = ({
+  setOwnerModel,
+  currency,
+  detail,
+  account,
+  setTransferModel,
+  setTransferCurrency,
+  setOpenDonate,
+  TOKEN_WITHDRAW,
+  setOpenUpdateAddress,
+  setOpenUpdatePrice,
+}) => {
+  return (
+    <section className="team pos-rel">
+      <div className="container">
+        <div className="new-owner team__wrap ul_li">
+          <div className="team__item">
+            <div className="avatar">
+              <img src="assets/img/shape/c_shape1.png" alt="" />
+            </div>
+            <div className="team__info text-center mb-20">
+              <h3>TOKEN TRANSFER</h3>
+              <span>Any ERC 20</span>
+            </div>
+            <div
+              className="team__social ul_li_center"
+            >
+              <span
+                onClick={() => (setOwnerModel(false), setTransferModel(true))}
+                className="h-icon"
+                style={{ cursor: "pointer" }}
+              >
+                <FaPlus />
+              </span>
+            </div>
           </div>
-          <div className="team__info text-center mb-20">
-            <h3>TOKEN TRANSFER</h3>
-            <span>Any ERC 20</span>
+          <div className="team__item">
+            <div className="avatar">
+              <img src="assets/img/token/t_info_img.png" alt="" />
+            </div>
+            <div className="team__info text-center mb-20">
+              <h3>TRANSFER FUND</h3>
+              <span>
+                {detail?.maticBal.slice(0, 6)}
+                {currency}
+              </span>
+            </div>
+            <div
+              className="team__social ul_li_center"
+            >
+              <span
+                onClick={() => (
+                  setOwnerModel(false), setTransferCurrency(true)
+                )}
+                className="h-icon"
+                style={{ cursor: "pointer" }}
+              >
+                <FaPlus />
+              </span>
+            </div>
           </div>
-          <div className="team__social"></div>
+          <div className="team__item">
+            <div className="avatar">
+              <img src="assets/img/shape/c_shape2.png" alt="" />
+            </div>
+            <div className="team__info text-center mb-20">
+              <h3>DONATE FUND</h3>
+              <span>If you can</span>
+            </div>
+            <div
+              className="team__social ul_li_center"
+            >
+              <span
+                onClick={() => (setOwnerModel(false), setOpenDonate(true))}
+                className="h-icon"
+                style={{ cursor: "pointer" }}
+              >
+                <FaPlus />
+              </span>
+            </div>
+          </div>
+
+          {account == detail?.owner && (
+            <>
+              <div className="team__item">
+                <div className="avatar">
+                  <img src="assets/img/token/t_info_img.png" alt="" />
+                </div>
+                <div className="team__info text-center mb-20">
+                  <h3>WITHDRAW</h3>
+                  <span>ICO Token, Only Owner</span>
+                </div>
+                <div className="team__social ul_li_center">
+                  <span
+                    onClick={() => TOKEN_WITHDRAW()}
+                    className="h-icon"
+                    style={{ cursor: "pointer" }}
+                  >
+                    <FaPlus />
+                  </span>
+                </div>
+              </div>
+              <div className="team__item">
+                <div className="avatar">
+                  <img src="assets/img/token/t_info_img.png" alt="" />
+                </div>
+                <div className="team__info text-center mb-20">
+                  <h3>UPDATE TOKEN</h3>
+                  <span>ICO Token, Only Owner</span>
+                </div>
+                <div className="team__social ul_li_center">
+                  <span
+                    onClick={() => (setOwnerModel(false), setOpenUpdateAddress(true))}
+                    className="h-icon"
+                    style={{ cursor: "pointer" }}
+                  >
+                    <FaPlus />
+                  </span>
+                </div>
+              </div>
+              <div className="team__item">
+                <div className="avatar">
+                  <img src="assets/img/token/t_info_img.png" alt="" />
+                </div>
+                <div className="team__info text-center mb-20">
+                  <h3>UPDATE TOKEN PRICE</h3>
+                  <span>ICO Token, Only Owner</span>
+                </div>
+                <div className="team__social ul_li_center">
+                  <span
+                    onClick={() => (setOwnerModel(false), setOpenUpdatePrice(true))}
+                    className="h-icon"
+                    style={{ cursor: "pointer" }}
+                  >
+                    <FaPlus />
+                  </span>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
-    </div>
-  </section>;
+
+      <div className="team__shape">
+        <div className="shape shape--1">
+          <img src="assets/img/shape/t_shape2.png" alt="" />
+        </div>
+        <div className="shape shape--1">
+          <img src="assets/img/shape/t_shape2.png" alt="" />
+        </div>
+      </div>
+
+
+    </section>
+  );
 };
 
 export default Owner;
