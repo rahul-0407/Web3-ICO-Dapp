cc409e98b0ddff6de4c424a693ea1be313e4d73c
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-02 13:19:02 +0530

    working on faq

--- Components/Faq.jsx
+++ Components/Faq.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Faq = () => {
-  return <div>Faq</div>;
+  return <section id="faq" className="faq pos-rel pt-140 pb-105">
+    <div className="container">
+      <div className="sec-title text-center mb-35">
+        <h5 className="sec-title__subtitle">FAQ</h5>
+        <h2 className="sec-title__title"></h2>
+      </div>
+    </div>
+  </section>;
 };
 
 export default Faq;
