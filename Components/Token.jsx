8123ee4d0aa11d003174f7b477d8538ab3b583db
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-03 00:58:11 +0530

    working token

--- Components/Token.jsx
+++ Components/Token.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Token = () => {
-  return <div>Token</div>;
+  return (
+    <section className="token pt-125">
+      <div className="container">
+        <div className="row align-items-center mt-none-30">
+          <div className="col-lg-5 mt-30">
+            <div className="token__content wow fadeInLeft">
+              <div className="section-title mb-20">
+                <h5 className="sec-title__subtitle">Tokenomics</h5>
+                <h2 className="sec-title__title">Token allocation & funds distribution</h2>
+              </div>
+              <ul className="nav nav-tabs token__tabs" id="myTab" role="tablist">
+                <li className="nav-item" role="presentation"><button className="nav-link active" id="home-tab" data-bs-toggle="tab" type="button" data-bs-target="#home" role="tab" aria-controls="home" aria-selected="true">Funding Allocation</button></li>
+                <li className="nav-item" role="presentation"><button className="nav-link" id="profile-tab" data-bs-toggle="tab" type="button" data-bs-target="#profile" role="tab" aria-controls="profile" aria-selected="true">Token Allocation</button></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Token;
