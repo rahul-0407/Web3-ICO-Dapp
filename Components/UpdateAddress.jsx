04feb2a32be05a40d8becf9f491a20395e56ce56
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-12-08 03:52:50 +0530

    updateAddress completed

--- Components/UpdateAddress.jsx
+++ Components/UpdateAddress.jsx
@@ -1,40 +1,75 @@
 import React, { useState, useEffect } from "react";
+import {shortenAddress} from "../Utils/index"
 
-const UpdateAddress = ({ detail, currency, setOpenUpdateAddress, UPDATE_TOKEN, ERC20, setLoader, }) => {
-  const [price, setPrice] = useState();
+const UpdateAddress = ({
+  detail,
+  currency,
+  setOpenUpdateAddress,
+  UPDATE_TOKEN,
+  ERC20,
+  setLoader,
+}) => {
+  const [address, setAddress] = useState();
+  const [tokenDetails, setTokenDetails] = useState();
+  const [transferToken, setTransferToken] = useState();
+
+  useEffect(() => {
+    if (transferToken) {
+      const loadToken = async () => {
+        setLoader(true);
+        const token = await ERC20(transferToken);
+        if (token == undefined) {
+          console.log("Kindly paste the token address");
+        } else {
+          setTokenDetails(token);
+          console.log(token);
+        }
+        setLoader(false);
+      };
+      loadToken();
+    }
+  }, [transferToken]);
 
   return (
     <section className="new-margin ico-contact pos-rel">
       <div className="container">
         <div className="ico-contact__wrap">
           <h2 className="title">
-            Update Token Price
-            <strong onClick={() => setOpenUpdatePrice(false)}>X</strong>
+            Update Token
+            <strong onClick={() => setOpenUpdateAddress(false)}>X</strong>
           </h2>
           <div>
             <div className="row">
               <div className="col-lg-12">
-                <input
-                  type="text"
-                  placeholder="_price"
-                  onChange={(e) => (
-                    setPrice(e.target.value )
-                  )}
-                />
+                {tokenDetails?.name ? (
+                  <input
+                    type="text"
+                    value={`Name ${tokenDetails?.name} Balance: ${tokenDetails?.balance} ${tokenDetails?.symbol}`}
+                  />
+                ) : (
+                  <input
+                    type="text"
+                    placeholder="_tokenAddress"
+                    onChange={(e) => (
+                      setAddress(e.target.value),
+                      setTransferToken(e.target.value)
+                    )}
+                  />
+                )}
               </div>
-
-              
 
               <p>
                 <strong>Current Price:</strong>
-                {detail?.tokenPrice} {currency} &nbsp; &nbsp; <strong>Token Balance:</strong> {detail?.tokenBal} {detail?.symbol}
+                {detail?.tokenPrice} {currency} &nbsp; &nbsp;{" "}
+                <strong>Token Balance:</strong> {detail?.tokenBal}{" "}
+                {detail?.symbol} &nbsp; &nbsp; <strong onClick={() => navigator.clipboard.writeText(detail?.tokenAddr)}>Token Address</strong> {detail?.tokenBal} {shortenAddress(detail?.tokenAddr)}
               </p>
               <div className="ico-contract__btn text-center mt-10">
                 <button
-                  onClick={() => UPDATE_TOKEN_PRICE(price)}
+                  onClick={() => UPDATE_TOKEN(address)}
                   className="thm-btn"
                 >
-                  Update Price
+                  Update Address
                 </button>
               </div>
             </div>
